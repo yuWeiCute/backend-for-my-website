ae1e05ea0b3a036170508b8b2c3a97d178c80e04
yuweiCute <jiang_yuwei@foxmail.com>
2022-03-01 15:28:25 +0800

    first commit

---
+++ schemas/pin.js
@@ -0,0 +1,68 @@
+export default {
+  name: 'pin',
+  title: 'Pin',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'destination',
+      title: 'Destination',
+      type: 'url',
+    },
+    {
+      name: 'github',
+      title: 'gitHub',
+      type: 'url',
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    },
+        {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'save',
+      title: 'Save',
+      type: 'array',
+      of: [{ type: 'save' }],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [{ type: 'comment' }],
+    },
+  ],
+};
