494c944dd8aa4d5863ea846df45a3fa392e39fe7
yuweiCute <jiang_yuwei@foxmail.com>
2022-05-26 21:41:57 +0800

    Revert "update"
    This reverts commit e491be56431f5f5a790027e188ab4bb90a0e96ba.

--- schemas/user.js
+++ schemas/user.js
@@ -1,32 +1,28 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    name: 'user',
-    title: 'User',
-    type: 'document',
-    fields: [{
-            name: 'userName',
-            title: 'UserName',
-            type: 'string',
-        },
-        {
-            name: 'email',
-            title: 'email',
-            type: 'string',
-        },
-        {
-            name: 'email',
-            title: 'email',
-            type: 'string',
-        },
-        {
-            name: 'platform',
-            title: 'platform',
-            type: 'string',
-        },
-        {
-            name: 'image',
-            title: 'Image',
-            type: 'string',
-        },
-    ],
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UserName',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'email',
+      type: 'string',
+    },
+    {
+      name: 'platform',
+      title: 'platform',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
 };
